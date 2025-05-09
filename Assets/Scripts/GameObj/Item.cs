dd9656a3f8567b7d8a4ef38d6c58ca7f3cf46774
Hicanos <ruda32168@gmail.com>
2025-05-09 14:50:50 +0900

    add; Egypt Items
    이집트 유물 추가

--- Assets/Scripts/GameObj/Item.cs
+++ Assets/Scripts/GameObj/Item.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public bool isDog;
+
+    //아이템/Trap: 뼈/생선 : 각각 Trigger가 개인지 고양이인지 확인
+    //알맞은 충돌 체에서 점수가 올라감. 아니라면 파괴되지 않음
+    //Gift: 유물. 획득하면 true. 각 스테이지 별로 3개를 모으면 도전과제 달성
+
+    void GetItem()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Trapped()
     {
-        
+
     }
+
+    void CheckDogorCat()
+    {
+
+    }
+
+    void GetGift()
+    {
+
+    }
+
 }
