593b4006177c8d20b8a21eefff7c278f7996db76
qorrud <son980503@gmail.com>
2025-05-12 17:35:58 +0900

    시간 관련 스크립트 작성
    최고시간 PlayerPrefs를 사용해서 기록하게끔

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
+    public static DataManager Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
 
     public static HashSet<int> AquiredLegacy = new HashSet<int>(); // 획득한 유물의 Key값만 저장, HashSet사용으로 중복제거
 
     public static int LegacyCount() //유물을 몇개 획득했는지 정리
     {
-        return AquiredLegacy.Count;        
+        return AquiredLegacy.Count;
     }
 
     public static Dictionary<int, string> LegacyList = new Dictionary<int, string>()
@@ -19,5 +33,9 @@ public class DataManager : MonoBehaviour
             {4, "페르세포네의 석류"}, {5, "삼지창"}, {6, "헤르메스의 팬티"},
             {7, "메긴요르드"}, {8, "묠니르"}, {9, "궁그닐"},
     };
+
+
+
+    
 
 }
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -18,10 +18,11 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        
-        //UIManager.Instance.InitUI();
-        //MapManager.Instance.LoadMap("스테이지 선택창");
-        //DataManager.Instance.LoadData();//점수와 같은 정보 호출
+    }
+    void StageClear()
+    {
+        UIManager.Instance.StopTimer();
+
     }
 }
 
